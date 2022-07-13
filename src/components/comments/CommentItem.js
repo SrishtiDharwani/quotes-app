ad5e65551975858145331b8774db1c43521e8956
SrishtiDharwani <srishti.dharwani.30@gmail.com>
2022-07-13 20:02:03 +0530

    quotes app completed

---
+++ src/components/comments/CommentItem.js
@@ -0,0 +1,22 @@
+// import classes from './CommentItem.module.css';
+
+// const CommentItem = (props) => {
+//   return (
+//     <li className={classes.item}>
+//       <p>{props.text}</p>
+//     </li>
+//   );
+// };
+
+// export default CommentItem;
+import classes from './CommentItem.module.css';
+
+const CommentItem = (props) => {
+  return (
+    <li className={classes.item}>
+      <p>{props.text}</p>
+    </li>
+  );
+};
+
+export default CommentItem;
